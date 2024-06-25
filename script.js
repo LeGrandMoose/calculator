9d425db6e99c50ac1ff17e4561b3b87a15027c46
Luca <47353869+LeGrandMoose@users.noreply.github.com>
2024-06-25 16:51:29 +0200

    implement operater function and create variabes

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ function divide(a,b){
     return a/b
 }
 
+let num1
+let num2
+let operator
+
+function operate(a,x,b){
+    if (x == '*'){
+        alert(multiply(a,b))
+    }
+    else if (x == '+'){
+        alert(add(a,b))
+    }
+    else if (x == '-'){
+        alert(subtract(a,b))
+    }
+    else if (x == '/'){
+        alert(divide(a,b))
+    }
+    else {
+        alert('Please input a valid operater ranging from +,-,*,/')
+    }
+}
+
+operate(10,'*',2)
+
